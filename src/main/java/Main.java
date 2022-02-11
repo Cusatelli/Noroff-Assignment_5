2f843d56d75f46d1d6d4913a4fad7b60ead26d22
Cusatelli <einarvandevelde@gmail.com>
2022-02-11 17:56:45 +0100

    chore(Main): added main

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+import controller.Controller;
+import controller.StateManager;
+import model.State;
+
+public class Main {
+    private boolean isRunning = true;
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
