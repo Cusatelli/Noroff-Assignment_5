a956462fd2a01ea1207f4597c87c60781bf7a3fd
Cusatelli <einarvandevelde@gmail.com>
2022-02-11 18:01:49 +0100

    refactor(Main): remove tag-along variable.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,6 @@ import controller.StateManager;
 import model.State;
 
 public class Main {
-    private boolean isRunning = true;
-
     public static void main(String[] args) {
         StateManager stateManager = StateManager.getInstance();
         Controller controller = Controller.getInstance();
