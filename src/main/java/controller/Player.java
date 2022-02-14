d33ae6ef60bc793c64b1c0b3b0ba3174d865e971
Cusatelli <einarvandevelde@gmail.com>
2022-02-14 14:51:06 +0100

    feat(Player): calculate how much damage the player does.

--- src/main/java/controller/Player.java
+++ src/main/java/controller/Player.java
@@ -3,6 +3,8 @@ package controller;
 import model.character.Character;
 import model.equipment.Equipped;
 import model.character.Role;
+import model.equipment.Weapon;
+import model.stats.Attributes;
 import model.stats.Stats;
 
 public class Player extends Character {
@@ -15,6 +17,21 @@ public class Player extends Character {
     public static Player getInstance() {
         if(instance == null) { instance = new Player(); }
         return instance;
+    }
+
+    public double getDamage() {
+        Weapon weapon =  this.getEquipment().getWeapon();
+        double weaponDamage = (weapon.getDamage() * weapon.getSpeed());
+        double statModifier = 0;
+        Attributes playerAttributes = getStats().getTotalAttributes();
+        switch (getRole()) {
+            case Deprived -> statModifier = (playerAttributes.getStrength() + playerAttributes.getDexterity()) / 1.5;
+            case Mage -> statModifier = playerAttributes.getIntelligence();
+            case Rogue, Ranger -> statModifier = playerAttributes.getDexterity();
+            case Warrior -> statModifier = playerAttributes.getStrength();
+        }
+
+        return weaponDamage + (weaponDamage * (statModifier / 100));
     }
 
     public Player(String name, Role role, Stats stats, Equipped equipment) {
