66755b813060cd051d76483c9a43f8f7da61d8ac
Cusatelli <einarvandevelde@gmail.com>
2022-02-15 14:43:41 +0100

    feat(Enemy): Added a new Enemy - Goblin

---
+++ src/main/java/controller/units/enemy/Goblin.java
@@ -0,0 +1,20 @@
+package controller.units.enemy;
+
+import model.character.Enemy;
+import model.item.equipment.Equipment;
+import model.item.equipment.Weapon;
+
+public class Goblin extends Enemy {
+    public Goblin(String name, int maxHealth, int damage, int experience) {
+        super(name, maxHealth, damage, experience);
+        this.setIntroduction(
+                "A wild '" + getName() + "' has appeared!" +
+                "\nYou prepare yourself before you..."
+        );
+    }
+
+    @Override
+    public Weapon getAttack() {
+        return new Attack("Stab", 1, 15, 1.5, Equipment.Weapon.Knuckles);
+    }
+}
