ce9c63fa8245e0bd3cd14cd8b71ef77909f1ec55
Cusatelli <einarvandevelde@gmail.com>
2022-02-15 16:38:02 +0100

    refactor(Armor): Replace Resilience with Attributes.

--- src/main/java/model/item/equipment/Armor.java
+++ src/main/java/model/item/equipment/Armor.java
@@ -1,21 +1,15 @@
 package model.item.equipment;
 
+import model.stats.Attributes;
+
 public abstract class Armor extends Equipment {
-    private int resistance;
+    private Attributes attributes;
     private Armor type;
 
-    public Armor(String name, int level, int resistance, Slot slot, Armor type) {
+    public Armor(String name, int level, Attributes attributes, Slot slot, Armor type) {
         super(name, level, slot);
-        this.resistance = resistance;
+        this.attributes = attributes;
         this.type = type;
-    }
-
-    public int getResistance() {
-        return resistance;
-    }
-
-    public void setResistance(int resistance) {
-        this.resistance = resistance;
     }
 
     @Override
@@ -24,7 +18,7 @@ public abstract class Armor extends Equipment {
                 "Name = '" + getName() + '\'' +
                 ", Level = " + getLevel() +
                 ", Slot = " + getSlot() +
-                ", Resistance = " + this.resistance +
+                ", Attributes = " + this.attributes +
                 " }";
     }
 
@@ -33,7 +27,7 @@ public abstract class Armor extends Equipment {
                 "\n\t\t\tName = '" + getName() + '\'' +
                 "\n\t\t\tLevel = " + getLevel() +
                 "\n\t\t\tSlot = " + getSlot() +
-                "\n\t\t\tResistance = " + this.resistance +
+                "\n\t\t\tAttributes = " + this.attributes.prettyPrint() +
                 "\n\t\t}";
     }
 }
