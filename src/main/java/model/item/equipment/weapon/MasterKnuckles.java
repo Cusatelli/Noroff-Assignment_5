d3ab15f35394e7e0f50ee22e14419633379c352a
Cusatelli <einarvandevelde@gmail.com>
2022-02-15 14:50:33 +0100

    feat(Equipment): Created MasterKnuckles

---
+++ src/main/java/model/item/equipment/weapon/MasterKnuckles.java
@@ -0,0 +1,9 @@
+package model.item.equipment.weapon;
+
+import model.item.equipment.Weapon;
+
+public class MasterKnuckles extends Weapon {
+    public MasterKnuckles() {
+        super("Master Knuckles", 5, 5, 5, Weapon.Knuckles);
+    }
+}
