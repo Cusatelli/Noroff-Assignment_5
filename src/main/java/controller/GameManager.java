b9f4aa8b203d210a56fb642f4ccd3a36c0b9a8e0
Cusatelli <einarvandevelde@gmail.com>
2022-02-15 19:44:00 +0100

    feat(GameManager): add gameState (decide combat or interaction), quality of life improvements, pricing items, equip item (& remove from inventory), display inventory/equipment/player stats.

--- src/main/java/controller/GameManager.java
+++ src/main/java/controller/GameManager.java
@@ -7,7 +7,25 @@ import model.State;
 import model.character.Enemy;
 import model.character.Role;
 import model.item.Item;
+import model.item.equipment.Armor;
+import model.item.equipment.Equipment;
+import model.item.equipment.Slot;
 import model.item.equipment.Weapon;
+import model.item.equipment.set.cloth.Cloth;
+import model.item.equipment.set.leather.Leather;
+import model.item.equipment.set.mail.Mail;
+import model.item.equipment.set.nude.Nude;
+import model.item.equipment.set.plate.Plate;
+import model.item.equipment.weapon.Knuckles;
+import model.item.equipment.weapon.MasterKnuckles;
+import model.item.equipment.weapon.axe.AdeptAxe;
+import model.item.equipment.weapon.axe.NoviceAxe;
+import model.item.equipment.weapon.bow.AdeptBow;
+import model.item.equipment.weapon.bow.NoviceBow;
+import model.item.equipment.weapon.dagger.AdeptDagger;
+import model.item.equipment.weapon.dagger.NoviceDagger;
+import model.item.equipment.weapon.staff.AdeptStaff;
+import model.item.equipment.weapon.staff.NoviceStaff;
 import view.Display;
 
 public class GameManager {
@@ -18,6 +36,7 @@ public class GameManager {
     private Player player = null;
     private boolean isPlayerTurn; // true - Player, false - Enemy
     private int interactionState = -1;
+    private int gameState = 0; // 0-4 Combat, 5 Interact
 
     private GameManager() { }
 
@@ -83,23 +102,9 @@ public class GameManager {
                     return;
                 }
 
-                int randomStartingPoint = randomRange(0, 8); // 0, 10 = 10% chance to encounter merchant
-                switch (randomStartingPoint) {
-                    case 0 -> {
-                        // Initialize NPC each time they are encountered.
-                        npc.initialize(Role.Rogue);
-                        npc.setRole(Role.Rogue);
-                        npc.setIntroduction(
-                                "A shady figure emerges from the depths of the woods." +
-                                "\nHe shakily asks you..." +
-                                "\nW.. W.. Wanna.. Bu.. uy.. some wares?! *cough*" +
-                                "\nKeh.. hehek.."
-                        );
-                        this.interactionState = -1;
-                        this.isPlayerTurn = false; // Always let NPC start interaction
-                        State.setInputState(State.Input.Interact);
-                    }
-                    case 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 -> {
+                //int randomStartingPoint = randomRange(0, 5); // 0, 10 = 10% chance to encounter merchant
+                switch (gameState) {
+                    case 0, 1, 2 -> {//, 3, 4 -> {
                         switch (randomRange(0, 10)) {
                             case 0, 1, 2, 3 -> { // 40%
                                 enemy = new Crow("Frenzied Crow", 15 * player.getLevel(), 5 * player.getLevel(), 5 * player.getLevel());
@@ -123,7 +128,24 @@ public class GameManager {
                         System.out.println(enemy.getIntroduction());
                         State.setInputState(State.Input.Combat);
                     }
+                    case 3 -> {
+                        // Initialize NPC each time they are encountered.
+                        npc.initialize(Role.Rogue);
+                        npc.setRole(Role.Rogue);
+                        npc.setIntroduction(
+                                "A shady figure emerges from the depths of the woods." +
+                                "\nHe shakily asks you..." +
+                                "\nW.. W.. Wanna.. Bu.. uy.. some wares?! *cough*" +
+                                "\nKeh.. hehek.."
+                        );
+
+                        this.interactionState = -1;
+                        this.isPlayerTurn = false; // Always let NPC start interaction
+                        State.setInputState(State.Input.Interact);
+                    }
                 }
+                gameState++;
+                if(gameState > 5) { gameState = 0; }
                 this.handleUserInput();
             }
             case Combat -> {
@@ -144,11 +166,12 @@ public class GameManager {
                             System.out.println(Display.PlayerDealDamageToEnemy(playerDamage, enemy.getName()));
                             System.out.println(Display.EnemyRemainingHealth(enemy.getName(), enemy.getCurrentHealth()));
                             if (enemy.isDead()) {
-                                int silverReward = randomRange(0, (int) Math.floor(1.5 * player.getLevel()));
+                                int silverReward = randomRange(1, player.getLevel());
                                 int expReward = enemy.getExperience();
                                 System.out.println(Display.EnemyVanquished(enemy.getName(), expReward, silverReward));
                                 player.levelUp(expReward);
                                 player.setSilver(player.getSilver() + silverReward);
+                                System.out.println(player.getSilver());
                                 enemy = null;
 
                                 State.setInputState(State.Input.Game); // TODO -> State.Input.Game
@@ -171,11 +194,15 @@ public class GameManager {
                             }
                         }
                         case 3 -> {
-                            System.out.println(player.getStats().toString());
+                            System.out.println(player.toString());
                             this.handleUserInput(); // Go back to top!
                         }
                         case 4 -> {
                             System.out.println(Display.Inventory(player.getInventoryAsString(), player.getSilver()));
+                            this.handleUserInput(); // Go back to top!
+                        }
+                        case 5 -> {
+                            System.out.println(player.getEquipment());
                             this.handleUserInput(); // Go back to top!
                         }
                         case 0 -> {
@@ -222,29 +249,42 @@ public class GameManager {
                     switch (option) {
                         case 1 -> {
                             int silverBalance = player.getSilver();
-                            System.out.println(Display.InteractShopMenu(silverBalance));
+                            int potionPrice = 5;
+                            int foodPrice = 2;
+                            int equipmentPrice = 2 * player.getLevel();
+                            if(equipmentPrice > 10) equipmentPrice = 10;
+                            System.out.println(Display.InteractShopMenu(potionPrice, foodPrice, equipmentPrice, silverBalance));
                             int interactOption = listen();
                             switch (interactOption) {
                                 case 1 -> {
-                                    int price = 5;
-                                    if(silverBalance >= price) {
-                                        player.setSilver(silverBalance - price);
+                                    if(silverBalance >= potionPrice) {
+                                        player.setSilver(player.getSilver() - potionPrice);
                                         player.addItemToInventory(new Item("Potion", player.getLevel(), randomRange(0, 20), Item.ItemType.Healing));
                                     }
                                 }
                                 case 2 -> {
-                                    int price = 2;
-                                    if(silverBalance >= price) {
-                                        player.setSilver(silverBalance - price);
+                                    if(silverBalance >= foodPrice) {
+                                        player.setSilver(player.getSilver() - foodPrice);
                                         player.addItemToInventory(new Item("Bread", player.getLevel(), randomRange(5, 10), Item.ItemType.Healing));
                                     }
                                 }
                                 case 3 -> {
-                                    int price = 2 * player.getLevel();
-                                    if(silverBalance >= price) {
-                                        player.setSilver(silverBalance - price);
-                                        player.addItemToInventory(new Item("Unknown", player.getLevel(), -1, Item.ItemType.Equipment, player.getRole()));//, Slot.get(randomRange(0, Slot.values().length))));
+                                    if(silverBalance >= equipmentPrice) {
+                                        player.setSilver(player.getSilver() - equipmentPrice);
+
+                                        // Generate "Random" equipment based on level and role
+                                        Slot slot = Slot.get(randomRange(0, 3));
+                                        Role role = this.player.getRole();
+                                        Equipment equipment = null;
+                                        if(this.player.getLevel() >= 5) { equipment = this.getAdept(role, slot); }
+                                        else { equipment = this.getNovice(role, slot); }
+                                        System.out.println(equipment);
+                                        player.addItemToInventory(equipment);
+                                        //player.addItemToInventory(new Item("Unknown", player.getLevel(), -1, Item.ItemType.Equipment, player.getRole()));//, Slot.get(randomRange(0, Slot.values().length))));
                                     }
+                                }
+                                case 4 -> {
+                                    System.out.println(player.getEquipment());
                                 }
                                 default -> {
                                     // Do nothing..
@@ -252,14 +292,36 @@ public class GameManager {
                             }
                             System.out.println(Display.Inventory(player.getInventoryAsString(), player.getSilver()));
                             interactionState = interactOption;
+                            this.handleUserInput();
                         }
                         case 2 -> {
-                            System.out.println(player.getStats().toString());
+                            System.out.println(player.toString());
                             this.handleUserInput(); // Go back to top!
                         }
                         case 3 -> {
                             System.out.println(player.getInventoryAsString());
+                            System.out.println("1. Use\n2. Equip\n0. Back");
+                            switch (this.listen()) {
+                                case 1 -> {
+                                    System.out.println("Heal..."); // TODO HEAL
+                                }
+                                case 2 -> {
+                                    Item equipmentItem = this.player.getFirstItemOfType(Item.ItemType.Equipment);
+                                    if(equipmentItem == null) {
+                                        System.out.println(Display.PlayerInventoryEquipmentItemNotFound());
+                                    } else {
+                                        this.player.getEquipment().set(equipmentItem);
+                                        this.player.getInventory().remove(equipmentItem);
+                                    }
+                                }
+                                case 0 -> {
+                                    this.handleUserInput();
+                                }
+                            }
                             this.handleUserInput(); // Go back to top!
+                        }
+                        case 4 -> {
+                            System.out.println(player.getEquipment());
                         }
                         case 0 -> {
                             State.setInputState(State.Input.Game); // TODO: State.setInputState(State.Input.Game);
@@ -282,6 +344,60 @@ public class GameManager {
             case Exit -> System.exit(0);
             default -> {}
         }
+    }
+
+    private Equipment getAdept(Role role, Slot slot) {
+        Armor armor = null;
+        switch (role) {
+            case Mage -> {
+                if(slot == Slot.Weapon) return new AdeptStaff();
+                armor = Cloth.Adept.Get(slot);
+            }
+            case Ranger -> {
+                if(slot == Slot.Weapon) return new AdeptBow();
+                armor = Mail.Adept.Get(slot);
+            }
+            case Rogue -> {
+                if(slot == Slot.Weapon) return new AdeptDagger();
+                armor = Leather.Adept.Get(slot);
+            }
+            case Warrior -> {
+                if(slot == Slot.Weapon) return new AdeptAxe();
+                armor = Plate.Adept.Get(slot);
+            }
+            default -> {
+                if(slot == Slot.Weapon) return new MasterKnuckles();
+                armor = Nude.Deprived.Get(slot);
+            }
+        }
+        return armor;
+    }
+
+    private Equipment getNovice(Role role, Slot slot) {
+        Armor armor = null;
+        switch (role) {
+            case Mage -> {
+                if(slot == Slot.Weapon) return new NoviceStaff();
+                armor = Cloth.Novice.Get(slot);
+            }
+            case Ranger -> {
+                if(slot == Slot.Weapon) return new NoviceBow();
+                armor = Mail.Novice.Get(slot);
+            }
+            case Rogue -> {
+                if(slot == Slot.Weapon) return new NoviceDagger();
+                armor = Leather.Novice.Get(slot);
+            }
+            case Warrior -> {
+                if(slot == Slot.Weapon) return new NoviceAxe();
+                armor = Plate.Novice.Get(slot);
+            }
+            default -> {
+                if(slot == Slot.Weapon) return new Knuckles();
+                armor = Nude.Deprived.Get(slot);
+            }
+        }
+        return armor;
     }
 
     private int randomRange(int min, int max) {
