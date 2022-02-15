e4c34ea681310d894159231baf6a7d2836b3c9ab
Cusatelli <einarvandevelde@gmail.com>
2022-02-15 14:43:00 +0100

    refactor(Enemy): added additional features such as enemy intro & exp on defeat.

--- src/main/java/model/character/Enemy.java
+++ src/main/java/model/character/Enemy.java
@@ -1,16 +1,22 @@
 package model.character;
 
+import model.item.equipment.Weapon;
+import model.item.equipment.weapon.Knuckles;
+
 public abstract class Enemy {
     private String name;
+    private String introduction = null;
     private int currentHealth;
     private int maxHealth;
     private int damage;
+    private int experience;
 
-    public Enemy(String name, int maxHealth, int damage) {
+    public Enemy(String name, int maxHealth, int damage, int experience) {
         this.name = name;
         this.maxHealth = maxHealth;
         this.currentHealth = maxHealth;
         this.damage = damage;
+        this.experience = experience;
     }
 
     public String getName() {
@@ -49,7 +55,31 @@ public abstract class Enemy {
         this.currentHealth -= damage;
     }
 
-    public void die() {
-        System.out.println("(WIP) The boar is dead...");
+    public void setIntroduction(String introduction) {
+        this.introduction = introduction;
+    }
+
+    public String getIntroduction() {
+        if(this.introduction == null) {
+            this.introduction = "A wild " + getName() + " has appeared!\nPrepare to battle!";
+        }
+        return this.introduction;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public void setExperience(int experience) {
+        this.experience = experience;
+    }
+
+    public Weapon getAttack() {
+        return new Knuckles();
+    }
+
+    public boolean isDead() {
+        return this.getCurrentHealth() <= 0;
     }
 }
+
