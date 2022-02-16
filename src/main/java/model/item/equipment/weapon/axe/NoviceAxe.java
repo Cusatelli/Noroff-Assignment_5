a01263eb4e500a0b6bdf76f6f62e95f76222de56
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 18:59:43 +0100

    docs(Weapon): documented Axe classes.

--- src/main/java/model/item/equipment/weapon/axe/NoviceAxe.java
+++ src/main/java/model/item/equipment/weapon/axe/NoviceAxe.java
@@ -2,7 +2,14 @@ package model.item.equipment.weapon.axe;
 
 import model.item.equipment.Weapon;
 
+/**
+ * Axe extends Weapon.
+ */
 public class NoviceAxe extends Weapon {
+
+    /**
+     * Initialize Axe Weapon (Novice Level).
+     */
     public NoviceAxe() {
         super("Novice Axe", 1, 12, 0.8, WeaponType.Axe);
     }
