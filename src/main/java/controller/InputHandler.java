463681a8ab52ad590d6ff50326c13573607512fb
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 19:54:03 +0100

    docs(InputHandler): documented InputHandler.

--- src/main/java/controller/InputHandler.java
+++ src/main/java/controller/InputHandler.java
@@ -2,12 +2,19 @@ package controller;
 
 import java.util.Scanner;
 
+/**
+ * Class handling user input.
+ */
 public class InputHandler {
     private static InputHandler instance = null;
     private Scanner scanner;
 
     private InputHandler() {}
 
+    /**
+     * Singleton
+     * @return InputHandler.
+     */
     public static InputHandler getInstance() {
         if(instance == null) {
             instance = new InputHandler();
@@ -15,12 +22,21 @@ public class InputHandler {
         return instance;
     }
 
+    /**
+     * Use Scanner to listen to user input from console.
+     * @return integer input.
+     */
     public int listen() {
         this.scanner = new Scanner(System.in);
         String input = this.scanner.nextLine();
         return parseInteger(input);
     }
 
+    /**
+     * Make sure input is a valid integer.
+     * @param input user input.
+     * @return integer.
+     */
     private int parseInteger(String input) {
         try {
             String trimmed = input.trim();
