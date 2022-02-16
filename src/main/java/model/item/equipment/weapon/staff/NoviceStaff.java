427736f294f968c8d050b25f0c991bfb70ce72c0
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 18:57:00 +0100

    docs(Weapon): documented Staff classes.

--- src/main/java/model/item/equipment/weapon/staff/NoviceStaff.java
+++ src/main/java/model/item/equipment/weapon/staff/NoviceStaff.java
@@ -2,7 +2,14 @@ package model.item.equipment.weapon.staff;
 
 import model.item.equipment.Weapon;
 
+/**
+ * Staff extends Weapon.
+ */
 public class NoviceStaff extends Weapon {
+
+    /**
+     * Initialize Staff Weapon (Novice Level).
+     */
     public NoviceStaff() {
         super("Novice Staff", 1, 10, 1, WeaponType.Staff);
     }
