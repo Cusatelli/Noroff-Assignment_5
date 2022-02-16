8ee42e7d063a563525fec32488f4b2b7dc9305c9
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 19:49:49 +0100

    docs(Exception): documented Exception.

--- src/main/java/exception/InvalidArmorException.java
+++ src/main/java/exception/InvalidArmorException.java
@@ -3,11 +3,24 @@ package exception;
 import model.character.Role;
 import model.item.equipment.Armor;
 
-public class InvalidArmorException  extends Exception {
+/**
+ * Exception to throw whenever an Armor can not be set.
+ */
+public class InvalidArmorException extends Exception {
+
+    /**
+     * Default exception to throw.
+     * @param message custom exception message.
+     */
     public InvalidArmorException(String message) {
         super(message);
     }
 
+    /**
+     * Custom exception to throw.
+     * @param role Role which can not equip armor.
+     * @param armor armor tried to be equipped.
+     */
     public InvalidArmorException(Role role, Armor armor) {
         super(role + " can not equip the armor " + armor.getName() + "!");
     }
--- src/main/java/exception/StateException.java
+++ src/main/java/exception/StateException.java
@@ -2,15 +2,33 @@ package exception;
 
 import model.State;
 
+/**
+ * Exception to throw whenever a State can not be set.
+ */
 public class StateException extends Exception {
+
+    /**
+     * Default exception to throw.
+     * @param message custom exception message.
+     */
     public StateException(String message) {
         super(message);
     }
 
+    /**
+     * Custom exception to throw.
+     * @param state The state which can not be resolved.
+     * @param c class name.
+     */
     public StateException(State.Controller state, Class<?> c) {
         super("Could not resolve State(Controller) -> " + state + " in, " + c.getName());
     }
 
+    /**
+     * Custom exception to throw.
+     * @param state The state which can not be resolved.
+     * @param c class name.
+     */
     public StateException(State.Input state, Class<?> c) {
         super("Could not resolve State(Input) -> " + state + " in, " + c.getName());
     }
