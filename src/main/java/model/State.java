3b872d290f43cd8e67645d1ae3801bbdc823f013
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 11:19:50 +0100

    refactor(State): move Combat & Interact States to State.

--- src/main/java/model/State.java
+++ src/main/java/model/State.java
@@ -24,6 +24,31 @@ public class State {
         Terminate
     }
 
+    public enum CombatState {
+        Escape, // Important that escape always is 0
+        Attack,
+        Heal,
+        Stats,
+        Inventory,
+        Equipment;
+
+        public static CombatState get(int index) {
+            return CombatState.values()[index];
+        }
+    }
+
+    public enum InteractState {
+        Leave, // Important that leave always is 0
+        Shop,
+        Stats,
+        Inventory,
+        Equipment;
+
+        public static InteractState get(int index) {
+            return InteractState.values()[index];
+        }
+    }
+
     public static void setInputState(Input state) {
         inputState = state;
     }
