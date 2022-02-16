ac88c367f27213e4574fe5ab34fb48d64d81d2d4
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 18:57:43 +0100

    docs(Weapon): documented Dagger classes.

--- src/main/java/model/item/equipment/weapon/dagger/NoviceDagger.java
+++ src/main/java/model/item/equipment/weapon/dagger/NoviceDagger.java
@@ -2,7 +2,14 @@ package model.item.equipment.weapon.dagger;
 
 import model.item.equipment.Weapon;
 
+/**
+ * Dagger extends Weapon.
+ */
 public class NoviceDagger extends Weapon {
+
+    /**
+     * Initialize Dagger Weapon (Novice Level).
+     */
     public NoviceDagger() {
         super("Novice Dagger", 1, 3, 3.4, WeaponType.Dagger);
     }
