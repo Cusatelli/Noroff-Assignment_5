94b7efa6223f050c7286b488fb54413bedf59677
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 18:59:05 +0100

    docs(Weapon): documented Bow classes.

--- src/main/java/model/item/equipment/weapon/bow/NoviceBow.java
+++ src/main/java/model/item/equipment/weapon/bow/NoviceBow.java
@@ -2,7 +2,14 @@ package model.item.equipment.weapon.bow;
 
 import model.item.equipment.Weapon;
 
+/**
+ * Bow extends Weapon.
+ */
 public class NoviceBow extends Weapon {
+
+    /**
+     * Initialize Bow Weapon (Novice Level).
+     */
     public NoviceBow() {
         super("Novice Bow", 1, 7, 1.4, WeaponType.Bow);
     }
