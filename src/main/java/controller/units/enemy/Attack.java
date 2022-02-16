c6fc28f9eacd010748266ffdb29f73c8d459ef05
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 19:51:14 +0100

    docs(Weapon): documented Attack Weapon.

--- src/main/java/controller/units/enemy/Attack.java
+++ src/main/java/controller/units/enemy/Attack.java
@@ -2,7 +2,19 @@ package controller.units.enemy;
 
 import model.item.equipment.Weapon;
 
+/**
+ * Attack extending Weapon used specifically by Enemies.
+ */
 public class Attack extends Weapon {
+
+    /**
+     * Initialize Attack
+     * @param name attack name.
+     * @param level attack level.
+     * @param damage attack damage.
+     * @param speed attack speed.
+     * @param type attack type.
+     */
     public Attack(String name, int level, int damage, double speed, WeaponType type) {
         super(name, level, damage, speed, type);
     }
