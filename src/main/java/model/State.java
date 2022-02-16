fd3b4ab6120c1b27605639502a99fa533a9a029c
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 16:15:30 +0100

    docs(State): documented State class & methods within.

--- src/main/java/model/State.java
+++ src/main/java/model/State.java
@@ -4,6 +4,9 @@ public class State {
     private static Controller controllerState = Controller.Initialize;
     private static Input inputState = Input.Menu;
 
+    /**
+     * Keep track of Controller States for updating the game loop.
+     */
     public enum Controller {
         Error,
         Initialize,
@@ -13,6 +16,9 @@ public class State {
         Terminate
     }
 
+    /**
+     * Keep track of Input State to know where to direct the user in console.
+     */
     public enum Input {
         Menu,
         Start,
@@ -24,6 +30,9 @@ public class State {
         Terminate
     }
 
+    /**
+     * Combat State to make switches more readable (Replaced 0-5 switch).
+     */
     public enum CombatState {
         Escape, // Important that escape always is 0
         Attack,
@@ -32,11 +41,20 @@ public class State {
         Inventory,
         Equipment;
 
+        /**
+         * Get the index of Combat States.
+         * Escape is always returned at 0.
+         * @param index which CombatState (in order) to return.
+         * @return CombatState
+         */
         public static CombatState get(int index) {
             return CombatState.values()[index];
         }
     }
 
+    /**
+     * Interact State to make switches more readable (Replaced 0-4 switch).
+     */
     public enum InteractState {
         Leave, // Important that leave always is 0
         Shop,
@@ -44,23 +62,45 @@ public class State {
         Inventory,
         Equipment;
 
+        /**
+         * Get the index of Interact States.
+         * Leave is always returned at 0.
+         * @param index which InteractState (in order) to return.
+         * @return InteractState
+         */
         public static InteractState get(int index) {
             return InteractState.values()[index];
         }
     }
 
+    /**
+     * Set Input State based on where the user shall be redirected after it's previous action.
+     * @param state enum of Input State.
+     */
     public static void setInputState(Input state) {
         inputState = state;
     }
 
+    /**
+     * Get Input State based.
+     * @return Input (State)
+     */
     public static Input getInputState() {
         return inputState;
     }
 
+    /**
+     * Set Controller State based on what state the "Engine"/Controller is in..
+     * @param state enum of Controller State.
+     */
     public static void setControllerState(Controller state) {
         controllerState = state;
     }
 
+    /**
+     * Get current Controller State.
+     * @return Controller (State)
+     */
     public static Controller getControllerState() {
         return controllerState;
     }
