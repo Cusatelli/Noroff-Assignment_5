4e960d75efd42e9f61e0f18e7a73482df75c6222
Cusatelli <einarvandevelde@gmail.com>
2022-02-16 18:54:43 +0100

    docs(Weapon): documented Knuckles classes.

--- src/main/java/model/item/equipment/weapon/Knuckles.java
+++ src/main/java/model/item/equipment/weapon/Knuckles.java
@@ -2,7 +2,14 @@ package model.item.equipment.weapon;
 
 import model.item.equipment.Weapon;
 
+/**
+ * Knuckles extends Weapon.
+ */
 public class Knuckles extends Weapon {
+
+    /**
+     * Initialize Knuckles Weapon (Novice Level).
+     */
     public Knuckles() {
         super("Bare Knuckles", 1, 1, 5, WeaponType.Knuckles);
     }
--- src/main/java/model/item/equipment/weapon/MasterKnuckles.java
+++ src/main/java/model/item/equipment/weapon/MasterKnuckles.java
@@ -2,7 +2,14 @@ package model.item.equipment.weapon;
 
 import model.item.equipment.Weapon;
 
+/**
+ * MasterKnuckles extends Weapon.
+ */
 public class MasterKnuckles extends Weapon {
+
+    /**
+     * Initialize Knuckles Weapon (Master Level).
+     */
     public MasterKnuckles() {
         super("Master Knuckles", 5, 5, 6, WeaponType.Knuckles);
     }
